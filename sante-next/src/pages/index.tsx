66d0dbb0f6eb7876bcf939d81bcf7e7733919415
LevyAM <levyzudo@msn.com>
2021-02-24 23:42:39 -0300

    Inseridas algumas regras de negócio

--- sante-next/src/pages/index.tsx
+++ sante-next/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css'
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -23,7 +24,7 @@ export default function Home() {
           <Countdown/>
         </div>
         <div>
-
+          <ChallengeBox/>
         </div>
       </section>
     </div>
